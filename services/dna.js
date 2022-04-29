85e7a1cee00f86b73e6d4a6d3920efaf6012e40d
edwin caicedo <comercial@somoscreandola.com>
2022-04-29 10:46:47 -0500

    create service with query for report

--- services/dna.js
+++ services/dna.js
@@ -1,10 +1,23 @@
 //conexión con la base de datos
 const { connection } = require("../config.db");
 
-exports.storeDna = async (dna) => {
+exports.getReport = async (cb) => {
+  connection.query(
+    "SELECT DISTINCT (SELECT COUNT(id) FROM dna WHERE dna.ismutant = 200) as count_mutant_dna, (SELECT COUNT(id) FROM dna WHERE dna.ismutant = 403) as count_human_dna, sum(case when dna.ismutant = 200 then 1 else 0 end)/count(*) as mutant_ratio, sum(case when dna.ismutant = 403 then 1 else 0 end)/count(*) as human_ratio FROM dna",
+    (error, results) => {
+      if (error) throw error;
+      cb(results);
+    }
+  );
+};
+
+exports.storeDna = async (dna, ismutant) => {
   let status = true;
   try {
-    await connection.query("INSERT INTO dna(dna) VALUES (?) ", [dna]);
+    await connection.query("INSERT INTO dna(dna, ismutant) VALUES (?, ?) ", [
+      dna,
+      ismutant,
+    ]);
   } catch (e) {
     status = false;
     console.log(e);
@@ -12,13 +25,13 @@ exports.storeDna = async (dna) => {
   return status;
 };
 
-exports.validateDnaExist = async (str, callback = null) => {
+exports.validateDnaExist = async (str, callback = null, status) => {
   connection.query(
     "select * from dna where dna = ?",
     [str],
     (error, results) => {
       if (error) throw error;
-      if (callback) callback(results.length > 0 ? true : false, str);
+      if (callback) callback(results.length > 0 ? true : false, str, status);
     }
   );
 };
