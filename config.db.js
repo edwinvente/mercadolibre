fc66046ca15497865d27177a182dfd48d6b41813
edwin caicedo <comercial@somoscreandola.com>
2022-04-29 17:50:39 -0500

    fix db conexion

--- config.db.js
+++ config.db.js
@@ -2,6 +2,44 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+/* module.exports = function () {
+  const mysql = require("mysql");
+  const db_config = {
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DBNAME,
+    port: process.env.DBPORT,
+  };
+
+  let connection;
+
+  function handleDisconnect() {
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+
+    connection.connect(function (err) {
+      if (err) {
+        console.log("error when connecting to db:", err);
+        setTimeout(handleDisconnect, 2000);
+      }
+    });
+
+    connection.on("error", function (err) {
+      console.log("db error", err);
+      if (err.code === "PROTOCOL_CONNECTION_LOST") {
+        // Connection to the MySQL server is usually
+        handleDisconnect();
+      } else {
+        throw err;
+      }
+    });
+  }
+
+  handleDisconnect();
+
+  return { connection };
+}; */
+
 const mysql = require("mysql");
 let connection;
 
