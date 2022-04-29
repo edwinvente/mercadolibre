d3cb4a187e7c3b36bc0e1504181bcc1d11730f68
edwin caicedo <comercial@somoscreandola.com>
2022-04-29 01:10:45 -0500

    create route for mutant

---
+++ routes/mutant.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+//conexión con la base de datos
+const { connection } = require("../config.db");
+const { getArticles, getTestMutant } = require("../controllers/mutant");
+
+//ruta
+app.route("/api/mutant").get(getArticles);
+app.route("/api/mutant").post(getTestMutant);
+
+const postCarta = (request, response) => {
+  const { plato, descripcion, precio, disponible } = request.body;
+  connection.query(
+    "INSERT INTO carta(plato, descripcion, precio, disponible) VALUES (?,?,?,?) ",
+    [plato, descripcion, precio, disponible],
+    (error, results) => {
+      if (error) throw error;
+      response
+        .status(201)
+        .json({ "Item añadido correctamente": results.affectedRows });
+    }
+  );
+};
+
+//ruta
+app.route("/carta").post(postCarta);
+
+const delCarta = (request, response) => {
+  const id = request.params.id;
+  connection.query("Delete from carta where id = ?", [id], (error, results) => {
+    if (error) throw error;
+    response.status(201).json({ "Item eliminado": results.affectedRows });
+  });
+};
+
+//ruta
+app.route("/carta/:id").delete(delCarta);
+
+module.exports = app;
